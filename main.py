2ecff9f43f0783f929146870a235dc7a8fbfb596
Pratikkk99 <pratikdiwte.com>
2025-03-24 11:54:45 +0530

    message

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hi")
+import streamlit as st
+
+def main():
+    st.set_page_config(page_title="Chat with PDFS",page_icon="books:")
+    st.header("Chat with PDFs :books:")
+    st.text_input("Ask a question about your documents:")
+
+    with st.sidebar:
+        st.subheader("Your Documents")
+        st.file_uploader("Upload your PDFs here and click on 'Process' ")
+        st.button("Process")
+
+
+if __name__=='__main__':
+    main()
